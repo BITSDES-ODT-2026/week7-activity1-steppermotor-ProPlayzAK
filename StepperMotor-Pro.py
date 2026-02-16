7b422e23b3bf97697db5ec9950e386c1afcd9509
Aakash Kadu <87316348+ProPlayzAK@users.noreply.github.com>
2026-02-16 15:42:44 +0530

    Update StepperMotor-Pro.py

--- StepperMotor-Pro.py
+++ StepperMotor-Pro.py
@@ -1,1 +1,27 @@
+from machine import Pin
+import time
 
+in1 = Pin(12, Pin.OUT)
+in2 = Pin(14, Pin.OUT)
+in3 = Pin(26, Pin.OUT)
+in4 = Pin(33, Pin.OUT)
+
+motor_list = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+reverse_list = [[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]]
+
+while True:
+    for x in range(500):
+        for i in motor_list:
+            in1.value(i[0])
+            in2.value(i[1])
+            in3.value(i[2])
+            in4.value(i[3])
+            time.sleep(0.003)
+            
+    for y in range(500):
+        for k in reverse_list:
+            in1.value(k[0])
+            in2.value(k[1])
+            in3.value(k[2])
+            in4.value(k[3])
+            time.sleep(0.003)
