ec1fdcbc2e055925e6b5d55ef50ae058d6951214
Aakash Kadu <87316348+ProPlayzAK@users.noreply.github.com>
2026-02-16 14:10:55 +0530

    Update StepperMotor-FullStep.py

--- StepperMotor-FullStep.py
+++ StepperMotor-FullStep.py
@@ -1,1 +1,17 @@
+from machine import Pin
+import time
 
+in1 = Pin(12, Pin.OUT)
+in2 = Pin(14, Pin.OUT)
+in3 = Pin(26, Pin.OUT)
+in4 = Pin(33, Pin.OUT)
+
+motor_list = [[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
+
+while True:
+    for i in motor_list:
+        in1.value(i[0])
+        in2.value(i[1])
+        in3.value(i[2])
+        in4.value(i[3])
+        time.sleep_ms(5)
